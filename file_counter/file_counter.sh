571500b5464757a02ca153edb43495a32e5b6fef
jschwartz <jadenmschwartz00@gmail.com>
2025-09-28 23:43:45 -0500

    fixed up the file counter

--- file_counter/file_counter.sh
+++ file_counter/file_counter.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 set +x
-helpmsg=" -d             specify a directory to count the files in\n ./file-counter.sh              displays this message.  "
-# this doesn't work.
-if [ ! -n "$0" ]; then
-    printf "%s$helpmsg"
+
+helpmsg="Usage:
+  $0 -d <directory>   Count the files in <directory>
+  $0                  Show this help message
+"
+
+# Show help if no args
+if [ $# -eq 0 ]; then
+    printf "%s" "$helpmsg"
+    exit 0
 fi
+
 if [ "$1" != "-d" ]; then
     echo "please use -d to specify the directory."
+    printf "%s" "$helpmsg"
+    exit 1
+fi
+
+directory=$2
+if [ -d "$directory" ]; then
+    echo "Counting the files in $directory ..."
+    num_files=$(find "$directory" -type f | wc -l)
+    echo "There are $num_files files in $directory."
 else
-    directory=$2
-    if [ -d "$directory" ]; then
-        echo "counting the files in $directory"
-        num_files=$(find "$directory" -type f | wc -l)
-        echo "There are "$num_files" in $directory"
-    fi
+    echo "Error: $directory is not a valid directory."
+    exit 1
 fi
